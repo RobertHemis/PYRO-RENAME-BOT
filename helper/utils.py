6593ab97ac9c76b49fd88b48f60975517338128f
Robert Hemis <130218888+RobertHemis@users.noreply.github.com>
2023-04-22 19:58:00 +0600

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -35,7 +35,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
             await message.edit(
                 text="{}\n\n{}".format(ud_type, tmp),               
                 reply_markup=InlineKeyboardMarkup( [[
-                    InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel")
+                    InlineKeyboardButton("Cancel", callback_data="cancel")
                     ]]
                 )
             )
